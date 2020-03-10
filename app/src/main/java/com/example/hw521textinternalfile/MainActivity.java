134e864bf0e60e5c88086ece2cf5ec420e4be41b
Dmitriy25 <ohapkin_da@mail.ru>
2020-03-10 09:32:53 +0300

    Рабочая версия

--- app/src/main/java/com/example/hw521textinternalfile/MainActivity.java
+++ app/src/main/java/com/example/hw521textinternalfile/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.hw521textinternalfile;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -9,7 +7,17 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
     private static final String FILE_NAME = "user_data.txt";
@@ -33,17 +41,111 @@ public class MainActivity extends AppCompatActivity {
         edtLogin = findViewById(R.id.edtLogin);
         edtPassword = findViewById(R.id.edtPassword);
 
-        btnRegistration.setOnClickListener(new View.OnClickListener() {
+        btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                if (TextUtils.isEmpty(edtLogin.getText()) || TextUtils.isEmpty(edtPassword.getText())) {
+                if (TextUtils.isEmpty(edtLogin.getText()) ||
+                        TextUtils.isEmpty(edtPassword.getText())) {
                     showToast(getString(R.string.error_empty_fields));
                     return;
                 }
 
-                FileOutputStream fileOutputStream = openFileOutput(FILE_NAME, MODE_PRIVATE);
+                String login = edtLogin.getText().toString();
+                String password = edtPassword.getText().toString();
 
+                FileInputStream fileInputStream = null;
+                try {
+                    fileInputStream = openFileInput(FILE_NAME);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+                if (fileInputStream == null) {
+                    showToast("Файл не найден"); //TODO вынести в ресурсы
+                    return;
+                }
+
+                InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                String line = null;
+                try {
+                    line = bufferedReader.readLine();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                while (line != null) {
+                    String[] splitLine = line.split(";");
+                    String curLogin = splitLine[0];
+                    String curPassword = splitLine[1];
+
+                    if (curLogin.equals(login) && curPassword.equals(password)) {
+                        showToast("Пользователь найден, вход выполнен успешно");
+                        return;
+                    }
+
+                    try {
+                        line = bufferedReader.readLine();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+
+                showToast("Неправильная комбинация логина и пароля");
+            }
+        });
+
+        btnRegistration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (TextUtils.isEmpty(edtLogin.getText()) ||
+                        TextUtils.isEmpty(edtPassword.getText())) {
+                    showToast(getString(R.string.error_empty_fields));
+                    return;
+                }
+
+                String login = edtLogin.getText().toString();
+                String password = edtPassword.getText().toString();
+
+                FileOutputStream fileOutputStream = null; //Выходной поток от нас , для записи в файл
+                try {
+                    if (new File(getFilesDir(), FILE_NAME).exists()) {
+                        fileOutputStream = openFileOutput(FILE_NAME, MODE_APPEND);
+                    } else {
+                        fileOutputStream = openFileOutput(FILE_NAME, MODE_PRIVATE);
+                    }
+
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                    //TODO Как может быть не найден файл, если он создаётся?
+                }
+
+                if (fileOutputStream == null) {
+                    showToast("Файл не найден"); //TODO вынести в ресурсы
+                    return;
+                }
+
+                //TODO почему не требуют обработки IO исключений
+                //Записыватель в этот выходной поток записи в файл
+                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream);
+                BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
+
+                try {
+                    bufferedWriter.write(login + ";" + password + "\n");
+                } catch (IOException e) {
+                    showToast(getString(R.string.error));
+                    e.printStackTrace();
+                }
+
+                try {
+                    bufferedWriter.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                showToast("Регистрация прошла успешно!");
 
             }
         });
